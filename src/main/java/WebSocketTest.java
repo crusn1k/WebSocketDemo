6709a6f781e7e4d56ffc3b55ae0ee8f708ffe500
Nishikant Ajay Patil <crusnik02.trinityblood@gmail.com>
2019-07-23 22:47:17 +0530

    changes to push data to all clients.

--- src/main/java/WebSocketTest.java
+++ src/main/java/WebSocketTest.java
@@ -1,29 +1,38 @@
-import javax.websocket.OnClose;
-import javax.websocket.OnError;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+import java.util.concurrent.ConcurrentHashMap;
 
 @ServerEndpoint("/test")
 public class WebSocketTest {
+    private final Logger logger = LogManager.getLogger(WebSocketTest.class);
+    private static final ConcurrentHashMap<String, Session> sessionMap = new ConcurrentHashMap<>();
 
     @OnOpen
-    public void onOpen(){
-        System.out.println("Connection opened ...");
+    public void onOpen(Session session) {
+        logger.info("Connection opened for {}", session.getId());
+        sessionMap.put(session.getId(), session);
     }
 
     @OnClose
-    public void onClose(){
-        System.out.println("Connection closed ...");
+    public void onClose(Session session) {
+        logger.info("Connection closed for {}", session.getId());
+        sessionMap.remove(session.getId());
     }
 
     @OnMessage
-    public String onMessage(String message){
-        return "Reply for : " + message;
+    public void onMessage(Session session, String message) {
+        sessionMap.forEach((id, _session) -> {
+            if (id.hashCode() != session.getId().hashCode() && !id.equals(session.getId())) {
+                _session.getAsyncRemote().sendText("Reply for : " + message);
+            }
+        });
     }
 
     @OnError
-    public void onError(Throwable e){
-        e.printStackTrace();
+    public void onError(Session session, Throwable e) {
+        logger.error(session.getId() + "\t" + e.getMessage(), e);
     }
 }
